69006ef3039bfb10f2789f67aa192947d5008db8
Shubham Gupta <shubhamg2208@live.com>
2020-01-02 23:46:59 +0800

    Could be a breaking change lol

--- question_answer.py
+++ question_answer.py
@@ -2,14 +2,21 @@ import requests
 import streamlit as st
 from transformers import pipeline
 
+from simpletransformers.question_answering import QuestionAnsweringModel
+
+model = QuestionAnsweringModel('bert', 'bert-base-cased', args=train_args)
+
 st.header("MICA: Your regulatory compliance assistant")
-url = st.text_input("Enter a url")
+#url = st.text_input("Enter a url")
+data = st.text_input("Enter text")
 question = st.text_input("Ask me a question")
+question_2 = st.text_input("Ask me another question")
 if st.button("Go"):
     nlp = pipeline("question-answering")
-    response = requests.get(url, verify=False)
+    #response = requests.get(url, verify=False)
     data = response.content
-    model_answer = nlp({"question": question, "context": str(data)})
-    score = float(model_answer["score"]) * 100
-    st.text_input("Answer", value=model_answer["answer"])
-    st.text_area("How confident am I about this answer?", value=f"{score:.2f}%")
+    model_answers = nlp({"question": [question, question_2], "context": str(data)})
+    for model_answer in model_answers:
+        score = float(model_answer["score"]) * 100
+        st.text_input("Answer", value=model_answer["answer"])
+        st.text_area("How confident am I about this answer?", value=f"{score:.2f}%")
